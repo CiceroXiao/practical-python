ad68463ac8860e97a8a59c270f8feb3479b11cee
CiceroXiao <xiaocicero@gmail.com>
2022-10-10 09:02:58 +0800

    昨天误解 2.7 习题的意思，因此 Cicero 提交了错误代码。 Cicero 修正了 2.7 习题的代码。

--- Work/report.py
+++ Work/report.py
@@ -2,38 +2,63 @@
 #
 # Exercise 2.4
 import csv
-import sys
-
-if len(sys.argv) == 2:
-    PORTFOLIO_PATH = sys.argv[1]
-else:
-    PORTFOLIO_PATH = (
-        r"/home/cicero/Documents/Projects/practical-python/Work/Data/portfolio.csv"
-    )
 
 
-def portfolio_cost(file_path) -> float | int:
-    """计算购买各公司股票的花费总额（ price * shares ）
+def read_portfolio(file_path):
+    """读取某个文件中的投资组合
     :param file_path: 存储购买信息的文件路径"""
     try:
         with open(file_path, mode="rt", encoding="utf-8") as f:
             rows = csv.reader(f)
-            stocks_header = next(rows)
-            stocks_info = [
-                {
-                    stocks_header[0]: row[0],
-                    stocks_header[1]: int(row[1]),
-                    stocks_header[2]: float(row[2]),
-                }
-                for row in rows
-            ]
+            try:
+                stocks_header = next(rows)
+                portfolio = [
+                    {
+                        stocks_header[0]: row[0],
+                        stocks_header[1]: int(row[1]),
+                        stocks_header[2]: float(row[2]),
+                    }
+                    for row in rows
+                ]
+            except ValueError as exc:
+                raise ValueError("文件内容有误，请您检查所提交文件的内容") from exc
 
-            total_cost = sum(stock["price"] * stock["shares"] for stock in stocks_info)
-            return total_cost
+            return portfolio
     except FileNotFoundError as exc:
-        raise FileNotFoundError("您提交的文件路径有误，请检查您提交的文件路径") from exc
-    except ValueError as exc:
-        raise ValueError("您提供的文件内容有误，请检查您的文件内容") from exc
+        raise FileNotFoundError("文件不存在，请您检查提交的文件路径") from exc
 
 
-print(f"Total cost: {portfolio_cost(PORTFOLIO_PATH):0.2f}")
+def read_prices(file_path):
+    """从指定文件中获取股票名称及其对应的价格
+    :param file_path: 股票文件的路径"""
+    try:
+        prices = {}
+        with open(file_path, mode="rt", encoding="utf-8") as f:
+            rows = csv.reader(f)
+            try:
+                for row in rows:
+                    prices[row[0]] = float(row[1])
+            except ValueError as exc:
+                raise ValueError("文件内容有误，请您检查所提交文件的内容") from exc
+            except IndexError:
+                pass
+
+            return prices
+    except FileNotFoundError as exc:
+        raise FileNotFoundError("文件不存在，请您检查提交的文件路径") from exc
+
+
+PORTFOLIO_PATH = (
+    r"/home/cicero/Documents/Projects/practical-python/Work/Data/portfolio.csv"
+)
+portfolio_detail = read_portfolio(file_path=PORTFOLIO_PATH)
+portfolio_cost = sum(stock["shares"] * stock["price"] for stock in portfolio_detail)
+print(f"Total cost: {portfolio_cost:0.2f}")
+
+PRICES_PATH = r"/home/cicero/Documents/Projects/practical-python/Work/Data/prices.csv"
+prices_detail = read_prices(file_path=PRICES_PATH)
+current_value = sum(
+    stock["shares"] * prices_detail[stock["name"]] for stock in portfolio_detail
+)
+print(f"Current value: {current_value:0.2f}")
+print(f"Total gain: {current_value - portfolio_cost:0.2f}")
